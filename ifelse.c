6c424127978e2227a1ddba092c531e743bf5901e
Anjali Dayal <135808224+anjaldayal@users.noreply.github.com>
2023-09-13 11:07:34 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,226 @@
+// # include<stdio.h>
+// int main(){
+//     int a=5;
+//     int b=20;
+//     printf("%d",a+b);
+// }
+
+// # include<stdio.h>
+// int main(){
+//     int a=9;
+//     int b=6;
+//     printf("%d",a+b);
+// }
+
+// # include<stdio.h>
+// int main(){
+//     int a=7;
+//     float b=9.8;
+//     printf("%d%f",a+b);
+// }
+
+// // # include<stdio.h>
+// // int main(){
+// //     int a=13;
+// //     int b=4;
+// //     printf("%d%d",(13*4)/4);
+    
+// }
+
+// # include<stdio.h>
+// int main (){
+//     int a=-15;
+//     int b=6;
+//     printf("%d%d",(-15*4/6));
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//   int time = 22;
+//   if (time < 10) {
+//     printf("Good morning.");
+//   } else if (time < 20) {
+//     printf("Good day.");
+//   } else {
+//     printf("Good evening.");
+//   }
+//   return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//   int myNum = 10;
+  
+//   if (myNum > 0) {
+//     printf("10");
+//   } else if (myNum < 0) {
+//     printf("-10");
+//   } else {
+//     printf("0");
+//   }
+  
+//   return 0;
+// }
+
+// # include<stdio.h>
+// int main()
+// {
+// int m;
+// printf("type a number:");
+// scanf("%d",&m);
+
+// }
+
+// return 0;
+
+// # include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while(i<10){
+//         printf("%s\n","dayal");
+//         i++;
+//         }
+//     return 0;
+// }
+
+// # include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while(i<=20){
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// # include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while(i<=20)
+//     {
+//         if (i%2==0)
+//         {
+//             printf("%d\n",i);
+//             }
+//             else
+//             {
+//                 printf("%d\n",i+1);
+//             i=i+2;
+//         }
+//     }
+//             return 0;
+// }
+
+// # include<stdio.h>
+// int main()
+// {
+//     int i=11;
+//     while(i<=110)
+//     {
+//         printf("%d\n",i);
+//         i=i+11;
+//     }
+//     return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     int i,j;
+//     for(i=1;i<=5;i++){
+//         for (j=1;j<=10;j++){
+//             printf("%d ",i*j);
+//         }printf("\n");
+//     }
+// return 0;
+// }
+
+// # include<stdio.h>
+// int main(){
+//     int i=1;
+//     while(i==1){
+//         printf("%d\n",i);
+//         i=i-1;
+//     }
+//     return 0;
+// }
+
+// Write a program in C to display the first 10 natural numbers.
+// Expected Output
+// # include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while (i<=10)
+//     {
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+    
+// }
+
+//  Write a C program to compute the sum of the first 10 natural numbers.
+// Expected Output :
+// # include<stdio.h>
+// int main()
+// {
+//     int i=0;
+//     int sum=0;
+//     while (i<=10)
+//     {
+//         // printf("%d\n",i);
+//         sum=sum+i;
+//         i++;
+        
+//     }
+//     printf("%d",sum);
+//     return 0;
+    
+// }
+
+// Write a program in C to display n terms of natural numbers and their sum.
+// Expected Output :
+// # include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d%s",&n,"enter a number");
+//     int i=1;
+//     int sum=0;
+//     while (i<=n){
+//         sum=sum+i;
+//         i++;
+
+//     printf("%d",sum);
+//     }
+// return 0;
+// }
+
+// Write a program in C to read 10 numbers from the keyboard and find their sum and average.
+// Input the 10 numbers:
+
+// # include<stdio.h>
+// int main(){
+
+// }
+
+// ....loop--for loop--while loop--do while loop.....
+// # include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while(i<=5)
+//     {
+//      printf("anjali\n");
+//      i++;
+//     }
+
+// }
+
+
+
